73b720a59c643a7b0e2367e2c08d493e8624e1c3
Ryan Ford <ryanford@email.com>
2019-04-27 20:40:14 +0800

    commands and mentions are not queries

--- patterns/docs.lua
+++ patterns/docs.lua
@@ -15,21 +15,21 @@ local space = white ^ 1
 local mention = P {
    'mention',
    mention = (((1 - V'user') ^ 0 * space) + opt_space) * V'user',
-   user = C(P'<@' * R'09' ^ 1 * P'>') * space,
+   user = C(P'<@' * P'!' ^ -1 * R'09' ^ 1 * P'>') * space,
 }
 
 local command = opt_space * P'!docs'
 local query = P {
    'arg',
    arg = V'term' ^ -2,
-   term = opt_space * C(P(1 - (space + P';')) ^ 0),
+   term = opt_space * C(P(1 - (space + P';' + mention + command)) ^ 0),
 }
 
 local docs = Ct(mention ^ 0) * command * Ct(query)
 
 patterns.docs = Ct(P {
     'message',
-    message = ((V'not_docs' * space * Ct(docs)) + Ct(docs) + (V'not_docs' * (-docs))) ^ 1 * -1,
+    message = ((V'not_docs' * space * Ct(docs)) + Ct(docs) + (V'not_docs' * (-docs + P'!docs' * space))) ^ 1 * -1,
     not_docs = opt_space * (1 - docs) ^ 1,
  })
 
