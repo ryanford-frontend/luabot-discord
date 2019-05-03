3bf73791db75779d5d2a13269f72e5083ec28165
Ryan Ford <ryanford@email.com>
2019-05-03 22:17:12 +0800

    Add help text, allow search categories, 2nd query

--- commands/man.lua
+++ commands/man.lua
@@ -5,27 +5,47 @@ local sf = string.format
 local unpack = table.unpack
 
 local man = {
-   _SIGNATURE = '[mentions [, ...]] !man [, query]',
+   _SIGNATURE = '[mentions [, ...]] !man [, section or query [, subquery]]',
    _DESCRIPTION = 'Search for Linux manpages',
+   _HELP = [[
+The table below shows the section numbers of the manual  followed  by  the
+types of pages they contain.
+
+1   Executable programs or shell commands
+2   System calls (functions provided by the kernel)
+3   Library calls (functions within program libraries)
+4   Special files (usually found in /dev)
+5   File formats and conventions eg /etc/passwd
+6   Games
+7   Miscellaneous (including macro packages and conventions)
+8   System administration commands (usually only for root)
+]],
 }
-local mt = {__call = function(_, message)
+local mt = {__call = function(self, message)
    local man_pattern = Pattern:new{
       name = 'man',
       prefix = '!',
-      args = 1,
+      args = 2,
    }
 
    local matches = man_pattern:match(message.content) or {}
 
    for _, match in ipairs(matches) do
       local mentions, queries = unpack(match)
-      local query = queries[1]
-      query = query and urlencode(query) or ''
-      local response = sf(
-         '%s <https://linux.die.net/man/1/%s>',
-         concat(mentions, ' '),
-         query
-      )
+      local query, subquery = unpack(queries)
+      query = query ~= '' and query or nil
+      local response = '<https://linux.die.net/'
+      query = urlencode(query)
+      subquery = urlencode(subquery)
+      if tonumber(query) then
+         response = sf('%sman/%s/%s>', response, query, subquery)
+      elseif query then
+         subquery = subquery ~= '' and subquery or nil
+         response = sf('%ssearch/?q=%s>', '<https://www.die.net/', urlencode(concat({ query, subquery }, '+')))
+      else
+         response = sf('```text\n%s\n```', concat({ self._DESCRIPTION, self._SIGNATURE }, '\n\n'))
+      end
+
       message.channel:send(response)
    end
 end}
